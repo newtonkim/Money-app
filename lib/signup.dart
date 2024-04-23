9854395005e85fa33659aae3d4ce671ac138a49f
Newtonkim <knewts98@gmail.com>
2024-04-23 14:35:57 +0300

    add signup logic

--- lib/signup.dart
+++ lib/signup.dart
@@ -61,12 +61,53 @@ class SignUpPage extends StatelessWidget {
                    inputFile(label: "Password", obscureText: true) ,
                    inputFile(label: "Confirm Password", obscureText: true) 
                 ],
+              ),
+
+              Container(
+                 padding: const EdgeInsets.only(top: 3, left: 3),
+                      decoration:
+                        BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          border: const Border(
+                            bottom: BorderSide(color: Colors.black),
+                            top: BorderSide(color: Colors.black),
+                            left: BorderSide(color: Colors.black),
+                            right: BorderSide(color: Colors.black),
+
+                          )
+
+                        ),
+
+                        child: MaterialButton(
+                        minWidth: double.infinity,
+                        height: 60,
+                        onPressed: () {},
+                        color: const Color(0xff0095FF),
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+
+                        ),
+                        child: const Text(
+                          "Sign up", style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: Colors.white,
+
+                        ),
+                        ),
+
+                      ),
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[],
               )
-            ]
-          )
-        )
-      
-      ,)
+            ],
+          ),
+        ),
+      ),
     );
    }
 }
